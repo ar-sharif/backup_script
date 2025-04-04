df8e7364d2f804732639f5d597607a29c83ec061
Sharif <ah.sharif.biz@gmail.com>
2025-04-04 23:57:58 +0330

    feat: add filter for keeping 10 backup file for each

--- sender.sh
+++ sender.sh
@@ -10,3 +10,10 @@ mkdir "$project_path/.tmp/${name}"
 
 date_time=$(date +"%Y-%m-%d_%H-%M-%S")
 tar -czvf "$project_path/.tmp/${name}/${name}_${date_time}.tar.gz" -C "$path" .
+
+backup_files_count=$(ls -1 $project_path/.tmp/$name/*.tar.gz | wc -l)
+
+if [ $backup_files_count -gt 10 ]; then
+    OLDEST_FILE=$(ls $project_path/.tmp/$name/*.tar.gz | sort | head -n 1)
+    rm -rf "$OLDEST_FILE"
+fi
